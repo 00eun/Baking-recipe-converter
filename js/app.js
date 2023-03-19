e79d0c67f9c30227d9b7c4a59c5a2fd9f2367e04
youngeun <00eun1128@gmail.com>
2023-03-19 20:19:00 +0900

    기본량에서 결과값 출력하기

--- js/app.js
+++ js/app.js
@@ -12,17 +12,18 @@ function updateValue(e){
   // log.textContent = e.target.value;
   if (e.target === originNum){
     originNumSave = e.target.value;
-    calculating();
+    timesCalculating();
   } else if (e.target === chandgeNum){
     chandgeNumSave = e.target.value;
-    calculating();
+    timesCalculating();
   } else {
     alert('다시입력해주세요')
   }
 }
 
-function calculating (){
-  const cNumberCalculat = chandgeNumSave/originNumSave
+let cNumberCalculat; 
+function timesCalculating (){
+  cNumberCalculat = chandgeNumSave/originNumSave
   if (!(originNumSave === undefined || chandgeNumSave === undefined)){
     cNumberResult.textContent = `원래 레시피 개수 : ${originNumSave}
     변경 레시피 개수 : ${chandgeNumSave}
@@ -32,3 +33,28 @@ function calculating (){
   }
 }
 
+// 기본값에서 결과값 출력하기
+const originGram = document.querySelectorAll('.originGram');
+const resultGram = document.querySelectorAll('.resultGram');
+
+originGram.forEach(value => {
+  value.addEventListener('change', logEvent);
+});
+
+function logEvent(e) {
+  let userGram = e.target.value;
+  const targetId = e.currentTarget.parentElement.id;
+  console.log(targetId, userGram);
+  console.log(cNumberCalculat);
+  gramCalculating(userGram, targetId);
+}
+
+function gramCalculating(userGram, targetId) {
+  console.log(resultGram);
+  resultGram.forEach(gram => {
+    if (targetId === gram.parentElement.id){
+      gram.innerText = `${cNumberCalculat * userGram}`
+    }
+  });
+  console.log(cNumberCalculat * userGram);
+}
